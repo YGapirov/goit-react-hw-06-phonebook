d796cff29d03d3ec4ef65b97f8266821e1e342a3
Yurii <gapirov21@gmail.com>
2023-12-05 00:33:06 +0200

    add Redux-start

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,54 +1,40 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { GlobalStyle } from '../../GlobalStyle';
-import { nanoid } from 'nanoid';
-
 import { ContactList } from '../ContactList/ContactList';
 import { ContactForm } from '../ContactForm/ContactForm';
 import { Filter } from '../Filter/Filter';
 import { Container, Title, SubTitle } from './App.styled';
+import { nanoid } from 'nanoid';
+
+import { useSelector, useDispatch } from 'react-redux';
 
 const LsKey = 'contacts';
 
-//зовнішня функція, яку передаєм в useState
-const getInitialContacts = () => {
-  const savedContacts = window.localStorage.getItem(LsKey); //виклакається і відображається ЛС до монтування
-  return savedContacts !== null ? JSON.parse(savedContacts) : initialContacts; //тернарний
-};
-
-const initialContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
-
 export const App = () => {
-  const [contacts, setContacts] = useState(getInitialContacts);
-  const [filter, setFilter] = useState('');
+  const contacts = useSelector(state => state.contacts);
+  const filter = useSelector(state => state.filter);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     window.localStorage.setItem(LsKey, JSON.stringify(contacts));
-  }, [contacts]); //під капотом порівняється значення контакт попередні з введеним
+  }, [contacts]);
 
   const addContact = values => {
-    const contact = { ...values, id: nanoid() };
-
     if (contacts.some(contact => contact.name === values.name)) {
       alert(`${values.name} is already on contacts.`);
       return;
     }
 
-    setContacts(prevState => [...prevState, contact]);
+    //діспатч доставляє в store
+    dispatch({ type: 'ADD_CONTACT', payload: { ...values, id: nanoid() } });
   };
 
-  const updateFilter = values => {
-    setFilter(values);
+  const updateFilter = value => {
+    dispatch({ type: 'SET_FILTER', payload: value });
   };
 
   const handleDelete = contactId => {
-    setContacts(prevState =>
-      prevState.filter(contact => contact.id !== contactId)
-    );
+    dispatch({ type: 'DELETE_CONTACT', payload: contactId });
   };
 
   const visibleContacts = contacts.filter(contact => {
